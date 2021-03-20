e7b14a4b1182b5d31235bb7638d7f5cbd434eaac
Rivaldo1123 <ramanlewis@gmail.com>
2021-03-20 02:16:17 -0500

    Part 1 Completed

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ import os
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://project1:1123@localhost/project1'
+    SECRET_KEY = 'Som3$ec5etK*y'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://project1:1123@localhost/project1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = './app/static/uploads'
 
